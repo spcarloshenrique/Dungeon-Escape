78a0d4cc83c5463619e90a5c5a7b14e59934f33d
Carlos Henrique <carloshsp10@gmail.com>
2023-10-27 22:06:13 -0300

    Player Andando e Mapa lv1
    Ainda sem portas

---
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Animator _playerAnimator;
+    Rigidbody2D _playerRb;
+    CapsuleCollider2D _playerCollider;
+    Vector2 mov;
+
+    //internas
+    [SerializeField]
+    float playerSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
+        _playerCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimentacao();
+    }
+
+
+    void Movimentacao()
+    {
+        _playerRb.velocity = new Vector2(mov.x * playerSpeed, mov.y * playerSpeed);
+
+        if (mov.x != 0 || mov.y != 0)
+        {
+            _playerAnimator.SetFloat("X", mov.x);
+            _playerAnimator.SetFloat("Y", mov.y);
+
+            _playerAnimator.SetBool("Walking", true);
+        }
+        else
+        {
+            _playerAnimator.SetBool("Walking", false);
+        }
+
+
+
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        mov = inputValue.Get<Vector2>();
+    }
+}
